5bf647ae6db02d62e98217b544c83c3aea057d4c
SteveJaxon <stephen.lee.jackson+github@googlemail.com>
2018-01-20 23:50:00 +0000

    Adding a RESTful endpoint for posting data

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 const blockchain = require('./src/main/nodejs/blockchain/bigchainDb');
 const bigchainDB = new blockchain.BigchainDbConnection();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/weather/lon/:lon/lat/:lat', function (req, res) {
     let params = req.params;
     return bigchainDB.retrieveWeatherData(params.lon, params.lat)
         .then((data) => {
-            console.log(data);
             res.send(data)
+        })
+        .catch((err) => {
+            res.status(500).send({ error: err });
+        })
+});
+
+app.post('/weather', function (req, res) {
+    return bigchainDB.storeWeatherData(req.body)
+        .then((result) => {
+            res.send(result)
         })
         .catch((err) => {
             res.status(500).send({ error: err });
